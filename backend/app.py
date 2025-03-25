fa005a7a751ab9404bc0fb4e89b6545b28454527
loqmen-anani <loqmen.anani@hotmail.com>
2025-03-25 03:43:21 +0100

    1st

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,8 @@ from auth import auth_bp
 from posts import posts_bp
 
 app = Flask(__name__)
-CORS(app)  # Permet à React (front-end) de faire des requêtes
+CORS(app, origins="http://127.0.0.1:5173")
+
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(posts_bp, url_prefix="/posts")
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,7 @@ function Register() {
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:5000/auth/register", {
+            const res = await axios.post("http://127.0.0.1:5000/auth/register", {
                 username,
                 password,
             });
